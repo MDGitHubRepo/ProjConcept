7141107ddfd5ef6aed8d3ef16290165faf42ec66
MarcDowner <mhdow2000@yahoo.com>
2017-04-22 10:35:19 -0400

    Change project name to include IndividualUsers

---
+++ VisualStudioProjects/ProjConcept/ProjConcept/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjConcept.Startup))]
+namespace ProjConcept
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
